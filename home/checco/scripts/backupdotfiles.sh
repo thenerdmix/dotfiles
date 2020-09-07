c91ba57acbade492e45defc20c03a7a8d25a9e7a
fradeb <francescodebortoli2000@gmail.com>
2020-09-07 15:24:31 +0200

    Backup some dotfiles

---
+++ home/checco/scripts/backupdotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+shopt -s expand_aliases
+
+alias cfg="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=/" 
+cfg add -A ~/scripts 
+cfg diff-index --quiet HEAD || cfg commit -m "Backup some dotfiles" 
+cfg push origin master 
