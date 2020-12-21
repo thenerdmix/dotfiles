519677444bc0e2585d461e30230d1349820bfd88
fradeb <francescodebortoli2000@gmail.com>
2020-12-21 14:44:13 +0100

    Backup some dotfiles

--- home/checco/scripts/backupall.sh
+++ home/checco/scripts/backupall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "tasi"
+echo `date` >>/home/checco/log/sns_error.log
+echo `date` >>/home/checco/log/dot_error.log
+echo `date` >>/home/checco/log/sns.log
+echo `date` >>/home/checco/log/dot.log
 /home/checco/scripts/backupsns.sh 2>>/home/checco/log/sns_error.log >>/home/checco/log/sns.log
 /bin/su -s /bin/bash -c '/home/checco/scripts/backupdotfiles.sh' checco 2>> /home/checco/log/dot_error.log >>/home/checco/log/dot.log
