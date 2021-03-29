79eb0614b1b08ac5b96300ea6b4e7ac44a831337
fradeb <francescodebortoli2000@gmail.com>
2021-03-29 22:34:51 +0200

    custom redshift

---
+++ home/checco/.config/bumblebee-status/modules/custom_redshift.py
@@ -0,0 +1,38 @@
+import re
+import threading
+
+import core.module
+import core.widget
+import core.input
+import core.decorators
+
+import util.cli
+import util.format
+import util.location
+
+
+def get_redshift_value(module):
+    widget = module.widget()
+    f = open("/home/checco/log/current_temp.txt", "r")
+    stringa = f.readline().strip()
+    f.close()
+    widget.set("temp", stringa)
+    core.event.trigger("update", [widget.module.id], redraw_only=True)
+
+
+class Module(core.module.Module):
+    @core.decorators.every(seconds=1)
+    def __init__(self, config, theme):
+        super().__init__(config, theme, core.widget.Widget(self.text))
+
+        self.__thread = None
+
+    def text(self, widget):
+        val = widget.get("temp", "n/a")
+        return val
+
+    def update(self):
+        if self.__thread is not None and self.__thread.is_alive():
+            return
+        self.__thread = threading.Thread(target=get_redshift_value, args=(self,))
+        self.__thread.start()
