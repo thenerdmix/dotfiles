bb6df6cd766e6eab956255a38a411e79690733ea
fradeb <francescodebortoli2000@gmail.com>
2021-04-01 20:01:47 +0200

    Backup some dotfiles

--- home/checco/scripts/backupsns.sh
+++ home/checco/scripts/backupsns.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rclone copy --config /home/checco/.config/rclone/rclone.conf --copy-links --verbose --update --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --exclude .git/** --stats 1s /home/checco/casa gdrivesns:backup/home/casa
 rclone copy --config /home/checco/.config/rclone/rclone.conf --copy-links --verbose --update --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s /home/checco/wallpaper gdrivesns:backup/home/wallpaper
+#rclone copy --config /home/checco/.config/rclone/rclone.conf --copy-links --verbose --update --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s /home/checco/wallpaper gdrivesns:backup/home/music
